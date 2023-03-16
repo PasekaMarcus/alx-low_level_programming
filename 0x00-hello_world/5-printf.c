22855846c2e5e29c7be21b2a0939797ee59bea37
Paseka Sonamzi <pasekasonamzi@gmail.com>
2023-03-16 10:16:03 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -  a C program that prints a function followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
