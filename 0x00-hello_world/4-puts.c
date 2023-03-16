89f03a7b3f29b9048c37b0f6f3d8b2c9a3d77c94
Paseka Sonamzi <pasekasonamzi@gmail.com>
2023-03-16 09:23:30 -0700

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints exactly the function, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
