f37360b43107ad0fe10f3a16c542fc956bb48772
Paseka Sonamzi <pasekasonamzi@gmail.com>
2023-03-19 12:47:55 -0700

    file 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
  * main - entry point
@@ -9,7 +11,7 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-		putchar(a);
+		printf("%d", a);
 	putchar('\n');
 	return (0);
 }
