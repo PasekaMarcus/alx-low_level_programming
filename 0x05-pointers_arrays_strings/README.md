c933ae869cf3eda7c2321f48a3198e93eee97985
Paseka Sonamzi <pasekasonamzi@gmail.com>
2023-03-27 09:47:50 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my 0-reset_to_98.c readme
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int_putchar(char c);
+void reset_to_98(int*n);
+void swap_int(int*a, int*b);
+int_strlen(char*s);
+void _puts(char*s);
+void print_rev(char*s);
+void rev_string(char*s);
+void puts2(char*str);
+void puts_half(char*str);
+void print_array(int*a, char*src);
+char*_strcpy(char*dest, char*src);
+
+#endif
