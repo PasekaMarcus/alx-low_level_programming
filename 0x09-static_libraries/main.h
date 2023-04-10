aa737bd889f00400aa35b45b7255927bde3dfb9a
Paseka Sonamzi <pasekasonamzi@gmail.com>
2023-04-10 03:51:14 -0700

    modified main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -17,3 +17,4 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
